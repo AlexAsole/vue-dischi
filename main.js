b218d0371da4cad939d5b9c9d1a72181a5a9e602
Alex Giorgio Asole <alexgiorgio.asole@gmail.com>
2021-02-08 13:01:55 +0100

    esercizio svolto

--- main.js
+++ main.js
@@ -1,3 +1,26 @@
-$(document).ready(function() {
-	//Code
-});
+new Vue({
+  el: '#root',
+  data: {
+    diskList: [],
+    selected: ''
+  },
+  methods: {
+    filterGenre: function(e) {
+      if (e.genre !== this.selected && this.selected !== '') {
+        return false
+      } else {
+        return true
+      }
+    }
+  },
+  mounted() {
+    const self = this;
+    axios.get('https://flynn.boolean.careers/exercises/api/array/music')
+      .then(function(resp) { /* funziona anche un foreach con cui pusho ogni item in disklist, ma così e più snello,
+        senza ciclare la lista */ 
+        const disks = resp.data.response
+        self.diskList = disks
+      })
+  }
+})
+Vue.config.devtools = true;
